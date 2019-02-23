ad6714852b8ccdee32754ab2746849dea8aacb58
ajlopez <webmaster@ajlopez.com>
2019-02-23 18:03:25 -0300

    Create constant node

---
+++ test/nodes.js
@@ -0,0 +1,12 @@
+
+const geast = require('..');
+
+exports['create constant'] = function (test) {
+    const result = geast.constant(42, 'int');
+
+    test.ok(result),
+    test.equal(typeof result, 'object');
+    test.equal(result.value(), 42);
+    test.equal(result.type(), 'int');
+};
+
