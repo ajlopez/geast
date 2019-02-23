20086ff0d17fd72b34ed3be62e52adf1e03c21ed
ajlopez <webmaster@ajlopez.com>
2019-02-23 18:06:20 -0300

    Create variable node

--- test/nodes.js
+++ test/nodes.js
@@ -1,12 +1,21 @@
 
 const geast = require('..');
 
-exports['create constant'] = function (test) {
+exports['create constant node'] = function (test) {
     const result = geast.constant(42, 'int');
 
     test.ok(result),
     test.equal(typeof result, 'object');
     test.equal(result.value(), 42);
+    test.equal(result.type(), 'int');
+};
+
+exports['create variable node'] = function (test) {
+    const result = geast.variable('a', 'int');
+
+    test.ok(result),
+    test.equal(typeof result, 'object');
+    test.equal(result.name(), 'a');
     test.equal(result.type(), 'int');
 };
 
