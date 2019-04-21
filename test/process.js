77a7b2143a2334ab3f1bca9ebb17fe06158c9ab0
ajlopez <webmaster@ajlopez.com>
2019-04-21 18:49:47 -0300

    Additional test, asynchronous process

--- test/process.js
+++ test/process.js
@@ -17,6 +17,30 @@ function process(test, name) {
     test.ok(processed);
 }
 
+async function asyncProcess(test, name) {
+    let processed = false;
+    
+    let processor = {};
+    
+    processor["process" + name[0].toUpperCase() + name.substring(1)] = async function (node) {
+        processed = true;
+        test.equal(node.ntype(), name);
+        
+        const promise = new Promise((resolve, reject) =>
+            setTimeout(function () { resolve(true); }, 0)
+        );
+        
+        return promise;
+    };
+    
+    const result = await geast[name]().process(processor);
+    
+    test.strictEqual(result, true);
+    
+    test.ok(processed);
+    test.done();
+}
+
 exports['process with procesor'] = function (test) {
     process(test, 'constant');
     process(test, 'variable');
@@ -38,5 +62,28 @@ exports['process with procesor'] = function (test) {
     process(test, 'array');
     process(test, 'method');
     process(test, 'eval');
+};
+
+exports['async process with procesor'] = async function (test) {
+    await asyncProcess(test, 'constant');
+    await asyncProcess(test, 'variable');
+    await asyncProcess(test, 'argument');
+    await asyncProcess(test, 'arguments');
+    await asyncProcess(test, 'assign');
+    await asyncProcess(test, 'name');
+    await asyncProcess(test, 'property');
+    await asyncProcess(test, 'binary');
+    await asyncProcess(test, 'sequence');
+    await asyncProcess(test, 'conditional');
+    await asyncProcess(test, 'loop');
+    await asyncProcess(test, 'call');
+    await asyncProcess(test, 'for');
+    await asyncProcess(test, 'function');
+    await asyncProcess(test, 'continue');
+    await asyncProcess(test, 'break');
+    await asyncProcess(test, 'return');
+    await asyncProcess(test, 'array');
+    await asyncProcess(test, 'method');
+    await asyncProcess(test, 'eval');
 };
 
